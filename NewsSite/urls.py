4a345554de1a80dcfa37a11e085206882949ccf2
lir147 <120119304+lir147@users.noreply.github.com>
2025-06-18 20:54:00 +0500

    NewsPortal

--- NewsSite/urls.py
+++ NewsSite/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('news/', include('news.urls')),
+    path('admin/',admin.site.urls),
+    path('news/',include('news.urls')),
 ]
 
 
--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ArticleListView, add_comment
 
 urlpatterns = [
-    path('articles/', ArticleListView.as_view(), name='article_list'),
-    path('articles/<int:post_id>/comment/', add_comment, name='add_comment'),
+    path('articles/',ArticleListView.as_view(),name='article_list'),
+    path('articles/<int:post_id>/comment/',add_comment,name='add_comment'),
 ]
