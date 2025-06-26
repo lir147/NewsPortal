e76dea7effcaa73b615465fe30385fb4759d4dcc
lir147 <120119304+lir147@users.noreply.github.com>
2025-06-26 17:13:53 +0500

    NewsPortal

--- NewsSite/settings.py
+++ NewsSite/settings.py
@@ -47,6 +47,7 @@ ROOT_URLCONF = 'NewsSite.urls'
 
 TEMPLATES = [
     {
+        
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
