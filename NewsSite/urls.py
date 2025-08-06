4fb792530d4fde968eb76e50dbd22280a6814285
lir147 <120119304+lir147@users.noreply.github.com>
2025-08-06 13:24:09 +0500

    NewsPortal

--- NewsSite/urls.py
+++ NewsSite/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('news/', include('news.urls')),
+
+    # Важно! Подключите стандартные урлы для смены языка
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 # Оборачиваем в i18n_patterns для поддержки интернационализации
